b7b10ec43f6a7cc8a89293901ec5c5befe188aff
DiltaFebiana <febianadilta@gmail.com>
2021-03-06 09:03:11 +0700

    commited

---
+++ KuisMhsDiltaFebiana/src/Kuis11H/DiltaFebiana/sepedaMotor.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kuis11H.DiltaFebiana;
+
+/**
+ *
+ * @author HP
+ */
+public class sepedaMotor {
+   String merk;
+   boolean kontakMotor;
+   int kecepatanMotor;
+   
+   public sepedaMotor(){
+       
+   }
+   public sepedaMotor(String mrk){
+       merk = mrk;
+   }
+   void nyalakanMotor(boolean kontak){
+       
+   }
+   void matikanMotor(boolean kontak){
+       
+   }
+  void tambahKecepatan()
+   }
+
